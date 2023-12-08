d76b1d3ee0b4bddc4592b0af57123ed10e080c8b
Kyrne <johnnylam0914@gmail.com>
2023-12-08 22:56:40 +0100

    tests

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
@@ -8,7 +8,7 @@ class RegisterUser(CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UpdateUser(RetrieveDestroyAPIView):
+class UpdateUser(RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
