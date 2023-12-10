c899294bc191c3af8e6bfcd2ff9e7be67addae18
Kyrne <johnnylam0914@gmail.com>
2023-12-10 19:04:25 +0100

    authorization for auction api

--- website/views/auction.py
+++ website/views/auction.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from website.serializer import (AuctionListSerializer,
                                 AuctionDetailSerializer,
@@ -11,7 +12,6 @@ from rest_framework.permissions import BasePermission
 
 
 class AuctionPermission(BasePermission):
-
     def has_permission(self, request, view):
         if view.action == "list":
             return True
@@ -19,10 +19,30 @@ class AuctionPermission(BasePermission):
             return request.user.is_authenticated
 
 
+class AuctionEditPermission(BasePermission):
+    def has_permission(self, request, view):
+        # staff and superuser can do anything
+        if request.user.is_staff or request.user.is_superuser:
+            return True
+        if request.method == "GET":
+            return True
+        return False  # you should not be able to edit any auctions
+
+    def has_object_permission(self, request, view, obj):
+        # staff and superuser can do anything
+        if request.user.is_staff or request.user.is_superuser:
+            return True
+        if request.method == "GET":
+            return True
+        return False  # you should not be able to edit any auctions
+        # if you want to be able to edit your auctions, use this:
+        # return obj.auctionOwner == request.user
+
+
 class AuctionList(ModelViewSet):
     queryset = Auction.objects.all()
     serializer_class = AuctionListSerializer
-    permission_classes = (AuctionPermission,)
+    permission_classes = (AuctionPermission, )
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -31,6 +51,8 @@ class AuctionList(ModelViewSet):
 
 
 class AuctionDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (AuctionEditPermission, )
+
     queryset = Auction.objects.all()
     serializer_class = AuctionDetailSerializer
     lookup_field = "auctionID"
@@ -52,10 +74,17 @@ class SubcategoryAuctionList(generics.ListAPIView):
         return Auction.objects.filter(subcategory__subcategory_name=subcat)
 
 
-class AuctionDeleteByUser (generics.DestroyAPIView):
+class MemberAuctionList(ModelViewSet):
     queryset = Auction.objects.all()
-    serializer_class = AuctionDetailSerializer
+    serializer_class = AuctionListSerializer
+    permission_classes = (AuctionEditPermission, )
 
     def get_queryset(self):
         username = self.kwargs["username"]
         return Auction.objects.filter(auctionOwner__username=username)
+
+    def delete(self, request, *args, **kwargs):
+        username = self.kwargs["username"]
+        resp_data = Auction.objects.filter(
+            auctionOwner__username=username).delete()
+        return Response(resp_data, status=status.HTTP_204_NO_CONTENT)
