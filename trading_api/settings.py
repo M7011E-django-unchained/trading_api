b2a60d1b20486dad28454b0624d7bb38cc49a3f9
casperlundberg <cappe5@hotmail.se>
2023-12-10 19:22:07 +0100

    no csrf auth but it works

--- trading_api/settings.py
+++ trading_api/settings.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Stockholm"
 
 USE_I18N = True
 
--- website/views/bid.py
+++ website/views/bid.py
@@ -1,33 +1,25 @@
 import datetime
 
+import pytz
 import requests
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from rest_framework.decorators import api_view
+from rest_framework.utils import json
 
 
 # Create your views here.
 @csrf_exempt
 @api_view(['POST'])
 def create_bid(request):
-    # print('request.body=<{}>'.format(request.body[:10]))
-    data = request.POST
-    # print('first few characters=<{}>'.format(data[:10]))
+    data = json.loads(request.body)
     bid = {
         "auctionId": data.get('auctionId'),
         "bidder": data.get('bidder'),
         "bidderId": data.get('bidderId'),
         "bidAmount": data.get('bidAmount'),
-        "bidTime": datetime.datetime.now(),
+        "bidTime": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
     }
-
-    # body_unicode = request.body.decode('utf-8')
-    # body = json.loads(body_unicode)
-    # bid = body['content']
-
-    # bid = request.body
-
-    # bid = json.loads(request.body)
 
     url = 'http://localhost:5000/api/v1/createBid'
     response = requests.post(url, json=bid)
@@ -42,28 +34,6 @@ def get_all_bids(request):
     return JsonResponse(data, safe=False)
 
 
-def get_all_bids_by_auction_id(request, id):
-    url = f'http://localhost:5000/api/v1/getAllBidsByAuctionId/{id}'
-    response = requests.get(url)
-    data = response.json()
-    return JsonResponse(data, safe=False)
-
-
-def get_all_bids_by_bidder_id(request, id):
-    url = f'http://localhost:5000/api/v1/getAllBidsByBidderId/{id}'
-    response = requests.get(url)
-    data = response.json()
-    return JsonResponse(data, safe=False)
-
-
-def get_all_bids_by_auction_id_and_bidder_id(request, auctionId, bidderId):
-    url = 'http://localhost:5000/api/v1/getAllbidsByAuctionIdAndBidderId/'
-    url += f'{auctionId}/{bidderId}'
-    response = requests.get(url)
-    data = response.json()
-    return JsonResponse(data, safe=False)
-
-
 def get_one_bid(request, _id):
     url = f'http://localhost:5000/api/v1/getOneBid/{_id}'
     response = requests.get(url)
@@ -71,20 +41,45 @@ def get_one_bid(request, _id):
     return JsonResponse(data, safe=False)
 
 
-def get_winnerby_auction_id(request, id):
-    url = f'http://localhost:5000/api/v1/getWinnerbyAuctionId/{id}'
+def get_all_bids_by_auction_id(request, auction_id):
+    url = f'http://localhost:5000/api/v1/getAllBidsByAuctionId/{auction_id}'
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data, safe=False)
 
 
+def get_all_bids_by_bidder_id(request, bidder_id):
+    url = f'http://localhost:5000/api/v1/getAllBidsByBidderId/{bidder_id}'
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data, safe=False)
+
+
+def get_all_bids_by_auction_id_and_bidder_id(request, auction_id, bidder_id):
+    url = 'http://localhost:5000/api/v1/getAllBids/'
+    url += f'{auction_id}/{bidder_id}'
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data, safe=False)
+
+
+def get_winnerby_auction_id(request, auction_id):
+    url = f'http://localhost:5000/api/v1/getWinnerbyAuctionId/{auction_id}'
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+@api_view(['POST'])
 def update_one_bid(request, _id):
+    data = json.loads(request.body)
     bid = {
-        "auctionId": request.POST.get('auctionId'),
-        "bidder": request.POST.get('bidder'),
-        "bidderId": request.POST.get('bidderId'),
-        "bidAmount": request.POST.get('bidAmount'),
-        "bidTime": request.POST.get('bidTime'),
+        "auctionId": data.get('auctionId'),
+        "bidder": data.get('bidder'),
+        "bidderId": data.get('bidderId'),
+        "bidAmount": data.get('bidAmount'),
+        "bidTime": data.get('bidTime'),
     }
 
     url = f'http://localhost:5000/api/v1/updateOneBid/{_id}'
@@ -93,6 +88,7 @@ def update_one_bid(request, _id):
     return JsonResponse(data, safe=False)
 
 
+@csrf_exempt
 def delete_one_bid(request, _id):
     url = f'http://localhost:5000/api/v1/deleteOneBid/{_id}'
     response = requests.delete(url)
@@ -100,8 +96,9 @@ def delete_one_bid(request, _id):
     return JsonResponse(data, safe=False)
 
 
-def delete_all_bids_by_auction_id(request, id):
-    url = f'http://localhost:5000/api/v1/deleteAllBidsByAuctionId/{id}'
+@csrf_exempt
+def delete_all_bids_by_auction_id(request, auction_id):
+    url = f'http://localhost:5000/api/v1/deleteAllBidsByAuctionId/{auction_id}'
     response = requests.delete(url)
     data = response.json()
     return JsonResponse(data, safe=False)
