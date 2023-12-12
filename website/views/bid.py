6453979a25fcf27896b0bc06a2a2ba8251ced70c
casperlundberg <cappe5@hotmail.se>
2023-12-12 21:08:17 +0100

    get_winner works but not robust
    Signed-off-by: casperlundberg <cappe5@hotmail.se>

--- website/views/bid.py
+++ website/views/bid.py
@@ -1,11 +1,10 @@
-import datetime, time
+import datetime
 import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.utils import json
 from website.models import Auction
-from website.views import AuctionDetail
 
 
 # Create your views here.
@@ -65,35 +64,14 @@ def get_all_bids_by_auction_id_and_bidder_id(request, auction_id, bidder_id):
 
 def get_winner_by_auction_id(request, auction_id):
     url = f'http://localhost:5000/api/v1/getWinnerbyAuctionId/{auction_id}'
-    # REAL CODE WHEN USER DATA AND AUCTION DATA ARE CREATED
-    # auction = Auction.objects.get(auctionID=auction_id)
-    # response = requests.get(url, json={"endTime": auction.endTime})
-
-    # WORKS BUT ARE NOT EFFECTIVE
-    # auction_url = f'http://localhost:8000/api/1/auction/{auction_id}'
-    # auction_response = requests.get(auction_url)
-    # auction = auction_response.json()
-    # end_time = auction.get('endTime')
-
     end_time = str(Auction.get_end_time(auction_id))[1:][:-1]
     print(end_time)
-
-    # STRUCTURE OF end_time IN BODY
-    # {
-    #     "endTime": "2023-12-12T08:07:08.049Z"
-    # }
-
-    # dt = datetime.datetime.fromisoformat(end_time.replace('Z', '+00:00'))
-    # for_js = int(time.mktime(end_time.timetuple())) * 1000
-
     body = {
         "endTime": end_time
     }
 
     response = requests.get(url, json=body)
 
-    # PLACEHOLDER CODE TO TEST CONNECTION
-    # response = requests.get(url, json={"endTime": "2023-12-12T08:07:08.049Z"})
     data = response.json()
     return JsonResponse(data, safe=False)
 
